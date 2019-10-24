47aefea14b902d4780c638f655c097b49c2d96f9
140ch204 <140ch204_github@gadz.org>
2019-10-24 20:01:34 +0200

    V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name,
+    description: Faker::Hacker.say_something_smart,
+    email: Faker::Internet.email,
+    age: rand(18..65)
+    )
+end
+
+City.destroy_all
+20.times do
+  City.create!(
+    name: Faker::Address.city, 
+    zip_code: Faker::Address.zip
+    )
+end
+
+Gosssip.destroy_all
+20.times do
+  Gosssip.create!(
+    title: Faker::Book.title, 
+    content: Faker::Hacker.say_something_smart
+    )
+end
